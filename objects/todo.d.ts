8498de75d17824ecddf400c7d30da946576bcdbc
pandajiny <astic1764@gmail.com>
2020-11-22 00:15:01 +0900

    ''

--- objects/todo.d.ts
+++ objects/todo.d.ts
@@ -1,7 +1,7 @@
 interface TodoItem extends DatabaseObject {
   content: string;
   isComplete: boolean;
-  parentId: string | nul;
+  parentId: string | null;
   createTime: number;
   end_time: number;
   group_id: string;
